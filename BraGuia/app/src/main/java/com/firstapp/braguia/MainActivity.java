94c3c2101848117d41dfe893802c9152fda0b1b2
andrenunes74 <61511913+andrenunes74@users.noreply.github.com>
2023-04-06 20:25:07 +0100

    nav bar prototype

--- BraGuia/app/src/main/java/com/firstapp/braguia/MainActivity.java
+++ BraGuia/app/src/main/java/com/firstapp/braguia/MainActivity.java
@@ -2,6 +2,7 @@ package com.firstapp.braguia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            getWindow().setNavigationBarColor(getResources().getColor(R.color.our_black));
+        }
     }
 }
