81da3b046c170aa529dcf1f785322035a8d5db01
andrenunes74 <61511913+andrenunes74@users.noreply.github.com>
2023-04-11 19:48:42 +0100

    login and register

--- BraGuia/app/src/main/java/com/firstapp/braguia/MainActivity.java
+++ BraGuia/app/src/main/java/com/firstapp/braguia/MainActivity.java
@@ -3,10 +3,20 @@ package com.firstapp.braguia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth auth;
+    Button button;
+    TextView textView;
+    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +27,22 @@ public class MainActivity extends AppCompatActivity {
 
         Button btn = (Button) findViewById(R.id.trailListButton);
         btn.setOnClickListener(v -> startActivity(new Intent(this, TrailListFragment.class)));
+
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+        //button = findViewById(R.id.logout);
+        //textView = findViewById(R.id.user_details);
+        if (user == null) {
+            Intent intent = new Intent(getApplicationContext(), Login.class);
+            startActivity(intent);
+            finish();
+        } else textView.setText(user.getEmail());
+
+        //button.setOnClickListener(new View.OnClickListener() {
+        //  @Override
+        // public void onClick(View view) {
+        //   FirebaseAuth.getInstance().signOut();
+        //}
+        //});
+        }
     }
-}
