b7b6c99b6506586e00eb95b31dd0d22408a06e4c
marcosampaio99 <marco.sampaio@live.com.pt>
2023-04-23 04:06:33 +0100

    all navigations working

--- BraGuia/app/src/main/java/com/firstapp/braguia/UI/TrailDetailsFragment.java
+++ BraGuia/app/src/main/java/com/firstapp/braguia/UI/TrailDetailsFragment.java
@@ -1,43 +1,96 @@
-package com.firstapp.braguia.UI;
+        package com.firstapp.braguia.UI;
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
+        import android.os.Bundle;
+        import android.view.LayoutInflater;
+        import android.view.MenuItem;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.Button;
+        import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
+        import androidx.annotation.NonNull;
+        import androidx.annotation.Nullable;
+        import androidx.fragment.app.Fragment;
+        import androidx.navigation.Navigation;
+        import androidx.navigation.fragment.NavHostFragment;
 
-import com.firstapp.braguia.Model.Trail;
-import com.firstapp.braguia.R;
-import com.google.android.gms.maps.MapView;
+        import com.firstapp.braguia.Model.Trail;
+        import com.firstapp.braguia.R;
+        import com.google.android.gms.maps.MapView;
+        import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class TrailDetailsFragment extends Fragment {
+        public class TrailDetailsFragment extends Fragment implements BottomNavigationView.OnItemSelectedListener{
 
-    private TextView trailTitle;
-    private MapView mapView;
-    private Button startTrailButton;
+            private TextView trailTitle;
+            private TextView trailDescription;
+            private MapView mapView;
+            private Button startTrailButton;
 
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.content_trail, container, false);
+            private BottomNavigationView bottomNavigationView;
 
-
-        trailTitle = view.findViewById(R.id.route_title);
-        //mapView = view.findViewById(R.id.map_view);
-        startTrailButton = view.findViewById(R.id.start_route_button);
-
-        Trail trail = (Trail) getArguments().getSerializable("selectedTrail");
-        trailTitle.setText(trail.getTrail_name());
-
-        // inicializar o mapview e tratar do botão de iniciar a rota
+            @Nullable
+            @Override
+            public View onCreateView( LayoutInflater inflater,ViewGroup container,  Bundle savedInstanceState) {
+                    View view = inflater.inflate(R.layout.content_trail, container, false);
 
 
-        return view;    }
+                    trailTitle = view.findViewById(R.id.route_title);
+                    trailDescription = view.findViewById(R.id.route_description);
+                    //mapView = view.findViewById(R.id.map_view);
+                    startTrailButton = view.findViewById(R.id.start_route_button);
 
-}
+                    Trail trail = (Trail) getArguments().getSerializable("selectedTrail");
+                    trailTitle.setText(trail.getTrail_name());
+                    trailDescription.setText(trail.getTrail_desc());
+
+                    // inicializar o mapview e tratar do botão de iniciar a rota
+
+                    bottomNavigationView = view.findViewById(R.id.bottom_navigation);
+                    bottomNavigationView.setOnItemSelectedListener(this);
+
+                    // Set the default selected item
+                    bottomNavigationView.setSelectedItemId(R.id.trailDetailsFragment);
+
+
+                    return view;
+                }
+
+
+
+
+
+
+
+
+
+
+
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.home:
+                        // Handle the click on the Home menu item
+                        navigateToHomeFragment();
+                        return true;
+                    case R.id.emergency:
+                        // Handle the click on the Emergency menu item
+                        EmergencyCall.makeEmergencyCall(getContext());
+                        return true;
+                    case R.id.route:
+                        //Handle the click on the Route menu item
+                        navigateToRouteFragment();
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+
+            private void navigateToHomeFragment() {
+                // Navigate to the HomeFragment
+                Navigation.findNavController(getView()).navigate(R.id.action_trailDetailsFragment_to_FistFragment);
+            }
+
+            private void navigateToRouteFragment() {
+                // Navigate to the HomeFragment
+                Navigation.findNavController(getView()).navigate(R.id.action_trailDetailsFragment_to_trailListFragment);
+            }
+        }
